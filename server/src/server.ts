1a4d19e4e116ad5ac50109bfdcf017ace9ddc982
Abel Gutierrez <32408235+abelgtzrs@users.noreply.github.com>
2025-04-22 18:05:12 -0400

    Added function to seed Users and Tickets to DB

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,8 @@ dotenv.config();
 import express from "express";
 import routes from "./routes/index.js";
 import { sequelize } from "./models/index.js";
+import { seedUsers } from "./seeds/user-seeds.js";
+import { seedTickets } from "./seeds/ticket-seeds.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +17,17 @@ app.use(express.static("../client/dist"));
 
 app.use(express.json());
 app.use(routes);
+
+app.get("/seed", async (_req, res) => {
+  try {
+    await seedUsers();
+    await seedTickets(); // optional
+    res.send("Database seeded!");
+  } catch (err) {
+    console.error("Seeding failed:", err);
+    res.status(500).send("Seeding failed");
+  }
+});
 
 sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
   app.listen(PORT, () => {
